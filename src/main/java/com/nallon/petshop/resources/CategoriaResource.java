94a8abd1ded29501c2d252ff8bbae88e580661b0
Nallon <nallonpb@gmail.com>
2023-03-06 20:36:37 -0300

    Adicionada classe de modelo categoria.

--- src/main/java/com/nallon/petshop/resources/CategoriaResource.java
+++ src/main/java/com/nallon/petshop/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.nallon.petshop.resources;
 
+import com.nallon.petshop.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
   @RequestMapping(method = RequestMethod.GET)
-  public String mostrar() {
-    return "Test REST categorias!";
+  public List<Categoria> mostrar() {
+    Categoria cat1 = new Categoria(1, "Alimento");
+    Categoria cat2 = new Categoria(2, "Cosmético");
+    Categoria cat3 = new Categoria(3, "Remédio");
+    List<Categoria> categorias= new ArrayList<>();
+    categorias.add(cat1);
+    categorias.add(cat2);
+    categorias.add(cat3);
+    return categorias;
   }
 }
